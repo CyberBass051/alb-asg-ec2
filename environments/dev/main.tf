c11e5f8a03cfeb69240e600c5b2233046a95a64c
CyberBass051 <pb05129000@tutamail.com>
2026-08-26 01:12:29 +0200

    Fix environments/dev/main.tf to comment out modules not yet committed

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -27,30 +27,30 @@ module "vpc" {
   }
 }
 
-module "security" {
-  source = "../../modules/security"
+# module "security" {
+#  source = "../../modules/security"
 
-  vpc_id       = module.vpc.vpc_id
-  project_name = "alb-project"
-  vpc_cidr     = "10.32.0.0/16"
-}
+#  vpc_id       = module.vpc.vpc_id
+#  project_name = "alb-project"
+#  vpc_cidr     = "10.32.0.0/16"
+# }
 
-module "alb" {
-  source = "../../modules/alb"
+# module "alb" {
+#  source = "../../modules/alb"
 
-  vpc_id             = module.vpc.vpc_id
-  project_name       = "alb-project"
-  owner              = "Pietro"
-  web_subnet_ids     = values(module.vpc.web_subnet_ids)
-  alb_sg_id          = module.security.alb_sg_id
-}
-
-module "dns_acm" {
-  source = "../../modules/dns_acm"
-
-  project_name        = "alb-project"
-  owner               = "Pietro"
-  domain_name         = "cyberbass.live"
-  lb_target_group_arn = module.alb.lb_target_group_arn
-  lb_arn              = module.alb.lb_arn
-}
+#  vpc_id             = module.vpc.vpc_id
+#  project_name       = "alb-project"
+#  owner              = "Pietro"
+#   web_subnet_ids     = values(module.vpc.web_subnet_ids)
+#   alb_sg_id          = module.security.alb_sg_id
+# }
+# 
+# module "dns_acm" {
+#   source = "../../modules/dns_acm"
+# 
+#   project_name        = "alb-project"
+#   owner               = "Pietro"
+#   domain_name         = "cyberbass.live"
+#   lb_target_group_arn = module.alb.lb_target_group_arn
+#   lb_arn              = module.alb.lb_arn
+# }
