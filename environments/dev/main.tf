73b7313df656f109a000fc22273b8003e9b8208d
CyberBass051 <pb05129000@tutamail.com>
2026-08-30 00:19:00 +0200

    feat(dns): Add modules/dns_acm, wired to environments/dev/main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -35,20 +35,20 @@ module "security" {
 }
 
 module "alb" {
-  source = "../../modules/alb"
-  vpc_id             = module.vpc.vpc_id
-  project_name       = "alb-project"
-  owner              = "Pietro"
-  web_subnet_ids     = values(module.vpc.web_subnet_ids)
-  alb_sg_id          = module.security.alb_sg_id
+  source         = "../../modules/alb"
+  vpc_id         = module.vpc.vpc_id
+  project_name   = "alb-project"
+  owner          = "Pietro"
+  web_subnet_ids = values(module.vpc.web_subnet_ids)
+  alb_sg_id      = module.security.alb_sg_id
 }
- 
-# module "dns_acm" {
-#   source = "../../modules/dns_acm"
-# 
-#   project_name        = "alb-project"
-#   owner               = "Pietro"
-#   domain_name         = "cyberbass.live"
-#   lb_target_group_arn = module.alb.lb_target_group_arn
-#   lb_arn              = module.alb.lb_arn
-# }
+
+module "dns_acm" {
+  source = "../../modules/dns_acm"
+
+  project_name        = "alb-project"
+  owner               = "Pietro"
+  domain_name         = "cyberbass.live"
+  lb_target_group_arn = module.alb.lb_target_group_arn
+  lb_arn              = module.alb.lb_arn
+}
---
+++ modules/dns_acm/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "lb_target_group_arn" {
+  type = string
+}
+
+variable "lb_arn" {
+  type = string
+}
