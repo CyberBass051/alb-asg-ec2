84af315b3e60706b82a99ef863200af532481df2
CyberBass051 <pb05129000@tutamail.com>
2026-09-06 20:14:45 +0200

    security(nlb): Fix Trivy scan: nlb to targets Security Group to allow VPC CIDR range only. Skipping Internet-Facing NLB security failure AWS-0053

--- modules/nlb/main.tf
+++ modules/nlb/main.tf
@@ -47,7 +47,7 @@ resource "aws_vpc_security_group_ingress_rule" "nlb_clients" {
 resource "aws_vpc_security_group_egress_rule" "nlb_clients" {
   security_group_id = aws_security_group.nlb.id
   ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = var.vpc_cidr
   description       = "Allows all traffic outbound"
 }
 
@@ -71,6 +71,7 @@ resource "aws_eip" "this" {
 # ==============
 
 resource "aws_lb" "this" {
+#trivy:ignore:AWS-0053 -- internet-facing NLB is the intended design (public static-IP front for the ALB)
   name               = "${var.project_name}-nlb"
   load_balancer_type = "network"
   internal           = var.internal
