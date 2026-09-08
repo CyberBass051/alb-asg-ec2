e2eae15adf0375dc9490f079632b5b872ad257cb
CyberBass051 <pb05129000@tutamail.com>
2026-09-09 01:00:00 +0200

    fix(security): add ALB SG egress to instances on 80 (empty egress blocked health checks)

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group_rule" "alb_egress_to_instances" {
   type                     = "egress"
   security_group_id        = aws_security_group.alb_sg.id
   description              = "Allow ALB to reach web instances (health checks + traffic)"
-  destination_security_group_id = aws_security_group.web_sg.id
+  source_security_group_id = aws_security_group.web_sg.id
   from_port                = 80
   to_port                  = 80
   protocol                 = "tcp"
