88714c8199dd4c98766880672a2aa1d1ee3f8ffd
CyberBass051 <pb05129000@tutamail.com>
2026-09-08 23:15:55 +0200

    fix(security): allow web tier egress to internet for dnf/SSM

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -64,9 +64,19 @@ resource "aws_security_group_rule" "web_allow_outbound_https" {
   type              = "egress"
   security_group_id = aws_security_group.web_sg.id
   description       = "Allow outbound HTTPS for software updates and APIs"
-  cidr_blocks       = [var.vpc_cidr]
+  cidr_blocks       = ["0.0.0.0/0"]
   from_port         = 443
   to_port           = 443
+  protocol          = "tcp"
+}
+
+resource "aws_security_group_rule" "web_allow_outbound_http" {
+  type              = "egress"
+  security_group_id = aws_security_group.web_sg.id
+  description       = "Allow outbound HTTP for software updates and APIs"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 80
+  to_port           = 80
   protocol          = "tcp"
 }
 
